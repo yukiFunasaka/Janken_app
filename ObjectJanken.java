eee4bd113826b2f01adc58778012da21b4252450
yukiFunasaka <ffffna0lucky@gmail.com>
2020-01-18 17:57:08 +0900

    second commit

---
+++ ObjectJanken.java
@@ -0,0 +1,15 @@
+public class ObjectJanken{
+    public static void main(String[] args){
+        Judge saito = new Judge();
+        Player kato = new Player("加藤さん");
+        Player suzuki = new Player("鈴木さん");
+
+        Tactics katoTactics = new StoneOnlyTactics();
+        kato.setTactics(katoTactics);
+
+        Tactics suzukiTactics = new RandomTactics();
+        suzuki.setTactics(suzukiTactics);
+
+        saito.startJanken(kato, suzuki);
+    }
+}
